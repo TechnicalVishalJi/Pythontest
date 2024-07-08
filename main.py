303d5c221696d0c7bf6e617abf215bb777c32cab
Vishal Singh <73434540+TechnicalVishalJi@users.noreply.github.com>
2024-07-08 15:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 app = Flask(__name__)
 
@@ -11,13 +12,10 @@ def home():
     options.add_argument("--headless")  # Run headless mode if you don't need to see the display output
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    title = "not found"
-    try:
-        driver.get("https://www.google.com")
-        title = driver.title
-        print(title)
-    finally:
-        driver.quit()
-        
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("https://www.google.com")
+    title = driver.title
+    print(title)
+    driver.quit()
     return f"Title of the page is: {title}"
